164f18868e2e56240c6f342913275c82dbaddc75
56314502 <56314502@L815WD220023.edu.cbe.ab.ca>
2023-09-19 15:32:24 -0600

    3:32 pm

--- Demojena/src/DemoTester.java
+++ Demojena/src/DemoTester.java
@@ -47,7 +47,7 @@ public class DemoTester {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 507, 382);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
@@ -57,19 +57,31 @@ public class DemoTester {
 		fn = new JTextField();
 		fn.setForeground(Color.LIGHT_GRAY);
 		fn.setText("First Name");
-		fn.setBounds(48, 13, 120, 38);
+		fn.setBounds(41, 13, 120, 38);
 		panel.add(fn);
 		fn.setColumns(10);
 		
+		JComboBox cb2 = new JComboBox();
+		cb2.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		cb2.setModel(new DefaultComboBoxModel(new String[] {"Select School", "Cresent", "Western", "Eastern"}));
+		cb2.setBounds(222, 81, 114, 28);
+		panel.add(cb2);
+		
+		JComboBox cb1 = new JComboBox();
+		cb1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		cb1.setModel(new DefaultComboBoxModel(new String[] {"Select Grade", "10", "11", "12"}));
+		cb1.setBounds(41, 81, 120, 28);
+		panel.add(cb1);
+		
 		JTextArea display = new JTextArea();
-		display.setBounds(48, 148, 250, 82);
+		display.setBounds(41, 134, 295, 117);
 		panel.add(display);
 		
 		ln = new JTextField();
 		ln.setForeground(Color.LIGHT_GRAY);
 		ln.setText("Last name");
 		ln.setColumns(10);
-		ln.setBounds(178, 13, 120, 38);
+		ln.setBounds(222, 13, 120, 38);
 		panel.add(ln);
 		
 		JButton btnNewButton = new JButton("Submit");
@@ -78,29 +90,51 @@ public class DemoTester {
 			{
 				String fN = fn.getText();
 				String lN = ln.getText();
+				String grade = "";
+				String school = "";
+				
+				if(cb1.getSelectedItem().equals("10"))
+				{
+					grade = "10";
+				}
+				else if(cb1.getSelectedItem().equals("11"))
+				{
+					grade = "11";
+				}
+				else if(cb1.getSelectedItem().equals("12"))
+				{
+					grade = "12";
+				}
+				
+				
+				if(cb1.getSelectedItem().equals("Cresent"))
+				{
+					school = "Cresent";
+				}
+				else if(cb1.getSelectedItem().equals("Western"))
+				{
+					school = "Western";
+				}
+				else if(cb1.getSelectedItem().equals("Eastern"))
+				{
+					school = "Eastern";
+				}
 				
 				display.setText
 				(
 					fN + " " +
-				    lN 
+				    lN + " is in grade" + 
+					grade + " and goes to" +
+				    school + " high school"
 						
 				);
 			}
 		});
-		btnNewButton.setBounds(329, 13, 95, 237);
+		btnNewButton.setBounds(365, 13, 116, 319);
 		panel.add(btnNewButton);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Select Grade", "10", "11", "12"}));
-		comboBox.setBounds(48, 81, 120, 28);
-		panel.add(comboBox);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Select School", "Cresent", "Western", "Eastern"}));
-		comboBox_1.setBounds(178, 81, 114, 28);
-		panel.add(comboBox_1);
+		
 		
 	
 	}
